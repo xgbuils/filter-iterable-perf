f6977e673a125839a1c5d168ec20bdf1f9c5b24b
Xavier Garcia Buils <xgbuils@gmail.com>
2017-08-22 23:31:04 +0200

    add new filter-arraylike-iterable releases

---
+++ perf/filter/tests/filter-arraylike-iterable_0.1.2.js
@@ -0,0 +1,12 @@
+const pkgVersion = 'filter-arraylike-iterable@0.1.2'
+const FilterArrayLikeIterable = require('../../packages')[pkgVersion]
+const traverse = require('../../traverse_iterable')
+
+module.exports = {
+    name: pkgVersion,
+    fn () {
+        const iterable = new FilterArrayLikeIterable(this.iterable)
+            .filter(this.even)
+        traverse(iterable)
+    }
+}
---
+++ perf/multiple_filter/tests/filter-arraylike-iterable_0.1.1.js
@@ -0,0 +1,15 @@
+const pkgVersion = 'filter-arraylike-iterable@0.1.1'
+const FunctorIterable = require('../../packages')[pkgVersion]
+const traverse = require('../../traverse_iterable')
+
+module.exports = {
+    name: pkgVersion,
+    fn () {
+        const n = this.numberOfMaps
+        let iterable = new FunctorIterable(this.iterable)
+        for (let i = 0; i < n; ++i) {
+            iterable = iterable.filter(e => e !== i)
+        }
+        traverse(iterable)
+    }
+}
